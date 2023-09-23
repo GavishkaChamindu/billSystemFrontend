3377a73ffa3c8f9a12632abd0497f0ffe9f603be
GavishkaChamindu <gavichamindu@gmail.com>
2023-09-23 18:35:17 +0530

    create details show page

--- src/pages/Customer/details/Details.jsx
+++ src/pages/Customer/details/Details.jsx
@@ -2,7 +2,18 @@ import React from "react";
 
 export default function Details() {
   return (
-    <div>
+    <div className="details">
+        <div className="d-left">
+            <div className="d-l-up">
+                <h1 className="bill-details">BILL DETAILS</h1>
+                <input type="text" placeholder="CEB Account Number" className="ac-no" />
+                <div className="proceed">
+                    <input type="submit" value="Proceed" className="proceed-btn" />
+                </div>
+            </div>
+            <div className="d-l-down"></div>
+        </div>
+        <div className="d-right"></div>
       
     </div>
   )
